3bf23c2244b32005a72c844ab4d8d682de5cacd6
KuangPF <1633397286@qq.com>
2023-05-20 16:40:25 +0800

    feat(config): integrate react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import AutoImport from 'astro-auto-import';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 import { autolinkConfig } from './pulgins/rehype-autolink-config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   markdown: {
     syntaxHighlight: 'prism',
     rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, autolinkConfig]],

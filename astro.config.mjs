0df7ae8a2a22caa2290aacfa106f8159e6d7aa35
KuangPF <1633397286@qq.com>
2023-05-24 23:32:50 +0800

    feat(mdx): integrate mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,24 @@ import AutoImport from 'astro-auto-import';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 import { autolinkConfig } from './pulgins/rehype-autolink-config';
+import { astroCodeSnippets, codeSnippetAutoImport } from './integrations/astro-code-snippets';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    AutoImport({
+      imports: [codeSnippetAutoImport],
+    }),
+    astroCodeSnippets(),
+    react(),
+    mdx(),
+  ],
   markdown: {
+    shikiConfig: {
+      theme: 'material-theme-palenight',
+    },
     rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, autolinkConfig]],
   },
 });

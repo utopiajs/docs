d6322f5916751b7eb2518499cf27e895d9bba1f0
KuangPF <1633397286@qq.com>
2023-05-25 19:44:50 +0800

    feat(page): 获取 guides 页面逻辑调整

--- src/content.ts
+++ src/content.ts
@@ -1,9 +1,3 @@
 import { getCollection } from 'astro:content';
 
-export const allGuidePages = await getCollection('guide', (entry) => {
-  if (import.meta.env.PUBLIC_TWO_LANG) {
-    return entry;
-  } else {
-    return true;
-  }
-});
+export const allGuidePages = await getCollection('guide', () => true);

8e45ba3f269a1aab809d3a618f794eb0742a17f3
KuangPF <1633397286@qq.com>
2023-05-26 21:12:18 +0800

    feat(component): `guide-list` add icon

---
+++ src/data/guideList.ts
@@ -0,0 +1,22 @@
+const guideList = {
+  fontend: {
+    title: '前端',
+    list: [
+      {
+        title: 'micro-main',
+        description: '微前端主平台，内置登陆、数据流、主题切换、个人中心等模块。',
+        github: 'https://github.com/utopiajs/micro-main',
+        tags: [{ type: 'template', name: '模板' }],
+      },
+      {
+        title: 'base-config',
+        description: '代码规范基础配置包，包含eslint，prettier，hooks规则等。',
+        github: 'https://github.com/utopiajs/base-config',
+        npm: "https://www.npmjs.com/package/@utopia/eslint-config-base",
+        tags: [{ type: 'config', name: '配置' }],
+      },
+    ],
+  },
+};
+
+export default guideList

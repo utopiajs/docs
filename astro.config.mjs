3581b84e713e58e0938704e6bc02dfa0dec35c26
KuangPF <1633397286@qq.com>
2023-05-28 16:01:00 +0800

    feat(component): add HeadSEO component

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://utopiajs.space/',
   integrations: [
     AutoImport({
       imports: [codeSnippetAutoImport],

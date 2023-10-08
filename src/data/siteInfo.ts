b472c13e4f609df15c03489959a24f7dc2773221
KuangPF <1633397286@qq.com>
2023-10-09 00:02:38 +0800

    docs(site): update site info

--- src/data/siteInfo.ts
+++ src/data/siteInfo.ts
@@ -1,10 +1,10 @@
 const siteInfo = {
   title: 'Upopia - 文档',
-  name: 'Utopia - 前端解决方案',
+  name: 'Utopia - 全流程、原子能力集技术解决方案',
   description:
     'Utopia 中文读音为“乌托邦”，意思为理想的国度。这里的 Utopia 不是一个框架，也不是一个库，可以将它理解为适用于团队或者个人的前端（不限于）解决方案，它可以提供技术架构、项目模板、代码规范配置以及服务部署等相关的方案。',
   author: 'KuangPF(匡鹏飞) | Utopia',
-  imageAlt: 'Utopia - 前端解决方案'
+  imageAlt: 'Utopia - 全流程、原子能力集技术解决方案'
 };
 
 export default siteInfo
